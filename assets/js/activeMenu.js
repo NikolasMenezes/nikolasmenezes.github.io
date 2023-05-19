af7815246669b5e659d0d06d847dc02323cb9cb7
Nikolas Menezes <nikolasdev.contato@gmail.com>
2023-05-18 22:39:49 -0300

    commit 18.05

---
+++ assets/js/activeMenu.js
@@ -0,0 +1,11 @@
+"use strict";
+const links = document.querySelectorAll(".navbar-link");
+const sections = document.querySelectorAll("section");
+const activeMenu = () => {
+    let len = sections.length;
+    while (--len && window.scrollY + 105 < sections[len].offsetTop) { }
+    links.forEach(link => link.classList.remove("current"));
+    links[len].classList.add("current");
+};
+activeMenu();
+window.addEventListener("scroll", activeMenu);
---
+++ assets/ts/activeMenu.ts
@@ -0,0 +1,16 @@
+const links: NodeListOf<HTMLAnchorElement> =  document.querySelectorAll(".navbar-link")
+const sections: NodeListOf<HTMLElement> =  document.querySelectorAll("section")
+
+
+const activeMenu = ():void =>{
+
+  let len:number = sections.length
+
+  while(--len && window.scrollY + 105 < sections[len].offsetTop){}
+  links.forEach( link => link.classList.remove("current"));
+  links[len].classList.add("current")
+}
+
+activeMenu();
+
+window.addEventListener("scroll", activeMenu)
